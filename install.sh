f0c7a1de27bec091ddf90c5434021503d3009d9b
fld <fld@r00t3d.com>
2017-07-01 23:30:50 +0300

    installer work

--- install.sh
+++ install.sh
@@ -5,14 +5,14 @@ git pull origin master
 git submodule init
 git submodule update
 
+for file in "$PWD"/.!(|.|git*); do echo "$file"; done
 read -rn1 -p "WARNING: Write config files under: $HOME/ (y/N): "
 [[ ! $REPLY =~ ^[Yy]$ ]] && exit 0; echo
+# Copy dotfiles under ~/
+for file in "$PWD"/.!(|.|git*); do ln -sfv "$file" ~; done
 
 # Set zsh as default shell
 chsh -s "$(which zsh)"
-
-# Link dotfiles to ~
-for file in "$PWD"/.!(|.|git*); do ln -sfv "$file" ~; done
 
 read -rn1 -p "Install Vim plugins? (Y/n): "
 [[ $REPLY =~ ^[Nn]$ ]] && exit 0; echo
