b72a86e306ef59b9264049539cf13c2bd2682389
fld <fld@r00t3d.com>
2017-07-01 23:33:49 +0300

    now?

--- install.sh
+++ install.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 shopt -s extglob # BASH Extended Globbing
 
+echo "Updating repos..."
 git pull origin master
 git submodule init
 git submodule update
+echo
 
+echo "dotfiles found:"
 for file in "$PWD"/.!(|.|git*); do echo "$file"; done
 read -rn1 -p "WARNING: Write config files under: $HOME/ (y/N): "
 [[ ! $REPLY =~ ^[Yy]$ ]] && exit 0; echo
 # Copy dotfiles under ~/
 for file in "$PWD"/.!(|.|git*); do ln -sfv "$file" ~; done
 
-# Set zsh as default shell
+echo "Setting zsh as default."
 chsh -s "$(which zsh)"
 
 read -rn1 -p "Install Vim plugins? (Y/n): "
