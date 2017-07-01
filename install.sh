2c6b2794f13739c8956d266828c70add3bfeacaf
fld <fld@r00t3d.com>
2017-07-01 23:25:22 +0300

    installer work

--- install.sh
+++ install.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 shopt -s extglob # BASH Extended Globbing
 
-read -rn1 -p 'git pull origin master? (Y/n): '
-[[ ! $REPLY =~ ^[Nn]$ ]] && git pull origin master; echo
+git pull origin master
+git submodule init
+git submodule update
 
-read -rn1 -p "WARNING: Overwrite config files under: $HOME/ (y/N): "
+read -rn1 -p "WARNING: Write config files under: $HOME/ (y/N): "
 [[ ! $REPLY =~ ^[Yy]$ ]] && exit 0; echo
 
 # Set zsh as default shell
 chsh -s "$(which zsh)"
 
 # Link dotfiles to ~
-for file in "$PWD"/.!(|.|git); do ln -sfv "$file" ~; done
+for file in "$PWD"/.!(|.|git*); do ln -sfv "$file" ~; done
+
+read -rn1 -p "Install Vim plugins? (Y/n): "
+[[ $REPLY =~ ^[Nn]$ ]] && exit 0; echo
 
 echo "Updating/cleaning Vim plugins:"
 vim -E -s <<-EOF
