5e51f612a48f8d306c5e4b4834d5bcc299ec05e2
fld <fld@r00t3d.com>
2019-10-07 00:00:20 +0300

    put some effort in tmpusb.sh

--- bin/tmpusb.sh
+++ bin/tmpusb.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
-tu_uuid="6F64-654D"                 # TmpUsb Partition UUID
-tu_key="/root/$(hostname)_root.key" # LUKS key location
+[ "$EUID" -ne 0 ] && { echo "Please run as root"; exit 1; }
 
+tu_key="/root/.tmpusb/$(hostname)_root.key" # LUKS keyfile location
+
+tu_uuid="6F64-654D" # TmpUsb Partition UUID
 tu_dev="/dev/disk/by-uuid/$tu_uuid"
 tu_unarmed="/dev/disk/by-label/Not\\x20Armed"
-if [[ -e $tu_key && -b $tu_dev && -b $tu_unarmed \
-        && $(readlink "$tu_dev") == $(readlink "$tu_unarmed") ]]; then
-    echo 'TmpUsb: Found Unarmed tmpusb. Copying key & Arming...'
-    mkdir /tmp/tmpusb || exit 1
-    mount "$tu_dev" /tmp/tmpusb || exit 1
-    chmod 700 /tmp/tmpusb || exit 1
-    cp "$tu_key" /tmp/tmpusb/ || exit 1
-    fatlabel "$tu_dev" Armed 2>/dev/null || exit 1
-    diff -q "$tu_key" "/tmp/tmpusb/$(basename "$tu_key")" &&
-        echo 'TmpUsb: Armed.'
-    umount /tmp/tmpusb/
-    rm -dI /tmp/tmpusb
+
+# Find unarmed TmpUsb and arm it with a keyfile
+if [[ -b $tu_dev ]]; then
+    [[ ! -e $tu_key ]] && { echo "TmpUsb: keyfile not found!"; exit 1; }
+    if [[ -b $tu_unarmed && $(readlink "$tu_dev") == $(readlink "$tu_unarmed") ]]; then
+        echo 'TmpUsb: Found Unarmed TmpUsb. Copying key & Arming...'
+        mkdir /root/.tmpusb/mnt || exit 1
+        chmod -R 700 /root/.tmpusb || exit 1
+        mount -o uid=0,gid=0,fmask=0177,dmask=0177 "$tu_dev" /root/.tmpusb/mnt || exit 1
+        cp "$tu_key" /root/.tmpusb/mnt/ || exit 1
+        fatlabel "$tu_dev" Armed 2>/dev/null || exit 1
+        diff -q "$tu_key" "/root/.tmpusb/mnt/$(basename "$tu_key")" &&
+            echo 'TmpUsb: Armed.'
+        umount /root/.tmpusb/mnt
+        rm -dI /root/.tmpusb/mnt
+    fi
+else
+    echo "TmpUsb not found!"
+    exit 1
 fi
